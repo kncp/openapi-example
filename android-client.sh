40816999ddf8a09667de930d30be3e3ad5310df5
kncp <k.goto.bn@gmail.com>
2019-05-23 17:46:23 +0900

    add scripts

---
+++ android-client.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+SCRIPT="$0"
+
+while [ -h "$SCRIPT" ] ; do
+  ls=`ls -ld "$SCRIPT"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    SCRIPT="$link"
+  else
+    SCRIPT=`dirname "$SCRIPT"`/"$link"
+  fi
+done
+
+if [ ! -d "${APP_DIR}" ]; then
+  APP_DIR=`dirname "$SCRIPT"`/..
+  APP_DIR=`cd "${APP_DIR}"; pwd`
+fi
+
+executable="npx openapi-generator"
+
+ags="generate -i openapi.yml -g java -c android-client.json -o dist/android-client -DuseRxJava=true,hideGenerationTimestamp=true $@"
+
+$executable $ags
